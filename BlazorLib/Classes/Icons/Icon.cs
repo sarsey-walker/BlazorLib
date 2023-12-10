c752478c5766111ef2b5c9329c23b8d46d55c025
Sarsey-walker <sarsey.james@gmailk.com>
2023-12-10 11:56:37 -0300

    Created libray of componets, icon and datepicker components

---
+++ BlazorLib/Classes/Icons/Icon.cs
@@ -0,0 +1,12 @@
+﻿
+namespace BlazorLib.Classes.Icons
+{
+    public class Icon
+    {
+        public const string ChevronLeft = "<path d=\"M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z\"/>";
+        public const string ChevronRight = "<path d=\"M0 0h24v24H0z\" fill=\"none\"/><path d=\"M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z\"/>";
+
+        public const string Event = "<path d=\"M0 0h24v24H0z\" fill=\"none\"/><path d=\"M17 12h-5v5h5v-5zM16 1v2H8V1H6v2H5c-1.11 0-1.99.9-1.99 2L3 19c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2h-1V1h-2zm3 18H5V8h14v11z\"/>";
+
+    }
+}
---
+++ BlazorLib/Components/Base/BaseComponent.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorLib.Components.Base
+{
+    public partial class BaseComponent
+    {
+        [Parameter]
+        public string? Style { get; set; }
+        [Parameter]
+        public string? Class { get; set; }
+    }
+}
