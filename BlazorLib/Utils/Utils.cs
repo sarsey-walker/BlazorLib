f23d35512402faaf291b340485bc4f443f4e3d99
Walker <sarsey.james@gmailk.com>
2023-12-11 13:45:12 -0300

    Created utilities

---
+++ BlazorLib/Utils/Utils.cs
@@ -0,0 +1,20 @@
+﻿
+namespace BlazorLib.Utils
+{
+    public static class Utils
+    {
+        public static string FirstToUpper(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return string.Empty;
+            }
+
+            return string.Create(input.Length, input, static (Span<char> chars, string str) =>
+            {
+                chars[0] = char.ToUpperInvariant(str[0]);
+                str.AsSpan(1).CopyTo(chars[1..]);
+            });
+        }
+    }
+}
