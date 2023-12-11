a5bf84edaf9f3e729ca158ab7bd231d6c644a4fb
Walker <sarsey.james@gmailk.com>
2023-12-11 13:44:59 -0300

    Improvment of datepicker

---
+++ BlazorLib/Components/DatePicker/CustomCalendar.razor.cs
@@ -0,0 +1,140 @@
+﻿using BlazorLib.Components.Base;
+using Microsoft.AspNetCore.Components;
+using System.Globalization;
+using BlazorLib.Components.Form;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace BlazorLib.Components.DatePicker
+{
+    public partial class CustomCalendar : FormBaseComponent<DateTime?>
+    {
+
+        /// <summary>
+        /// The currently selected date (two-way bindable). If null, then nothing was selected.
+        /// </summary>
+        [Parameter]
+        public DateTime? Date
+        {
+            get => _value;
+            set => SetDate(value, true);
+        }
+
+        /// <summary>
+        /// Defines on which day the week starts. Depends on the value of Culture.
+        /// </summary>
+        [Parameter]
+        public DayOfWeek? FirstDayOfWeek { get; set; } = null;
+
+        private DateTime currentDate = DateTime.Today;
+        private DateTime currentMonth => new DateTime(currentDate.Year, currentDate.Month, 1);
+        private int currentYear => currentMonth.Year;
+        private int daysInMonth => DateTime.DaysInMonth(currentYear, currentMonth.Month);
+
+        private List<DateTime> days = null!;
+
+        [Parameter] public EventCallback<DateTime?> SelectedDateChanged { get; set; }
+        [Parameter] public EventCallback<DateTime?> MonthChanged { get; set; }
+        [Parameter] public List<int>? MarkedDays { get; set; }
+
+
+        protected override void OnInitialized()
+        {
+            if(Date ==  null)
+            {
+                Date = DateTime.Today;
+            }
+            UpdateDays();
+        }
+
+
+        private void UpdateDays()
+        {
+            days = new List<DateTime>();
+            int firstDayOfWeek = (int)currentMonth.DayOfWeek;
+            for (int i = 0; i < firstDayOfWeek; i++)
+            {
+                days.Add(DateTime.MinValue);
+            }
+            for (int i = 0; i < daysInMonth; i++)
+            {
+                days.Add(new DateTime(currentYear, currentMonth.Month, i + 1));
+            }
+        }
+
+
+        private async void SelectDate(int index)
+        {
+            Date = days[index];
+            await SelectedDateChanged.InvokeAsync(Date);
+        }
+
+        private void PreviousMonth()
+        {
+            currentDate = currentDate.AddMonths(-1);
+            MonthChanged.InvokeAsync(currentDate);
+            UpdateDays();
+        }
+
+        private void NextMonth()
+        {
+            currentDate = currentDate.AddMonths(1);
+            MonthChanged.InvokeAsync(currentDate);
+            UpdateDays();
+        }
+
+        private string GetDayClasses(DateTime day)
+        {
+            if (day == DateTime.Today)
+            {
+                return "w-current-day";
+            }
+            return "";
+        }
+
+        protected void SetDate(DateTime? date, bool updateValue)
+        {
+            if (_value != null && date != null && date.Value.Kind == DateTimeKind.Unspecified)
+            {
+                date = DateTime.SpecifyKind(date.Value, _value.Value.Kind);
+            }
+
+            if (_value != date)
+            {
+                if (date is not null)
+                {
+                    _value = date;
+                    return;
+                }
+                _value = date;
+            }
+        }
+
+        /// <summary>
+        /// Get week names 
+        /// </summary>
+        /// <returns>return Mo, Tu, We, Th, Fr, Sa, Su in the right culture</returns>
+        protected IEnumerable<string> GetAbbreviatedDayNames()
+        {
+            var dayNamesNormal = CultureInfo.CurrentCulture.DateTimeFormat.AbbreviatedDayNames;
+            var dayNamesShifted = Shift(dayNamesNormal, (int)GetFirstDayOfWeek());
+            return dayNamesShifted;
+        }
+
+        protected DayOfWeek GetFirstDayOfWeek()
+        {
+            if (FirstDayOfWeek.HasValue)
+                return FirstDayOfWeek.Value;
+            return CultureInfo.CurrentCulture.DateTimeFormat.FirstDayOfWeek;
+        }
+        /// <summary>
+        /// Shift array and cycle around from the end
+        /// </summary>
+        private static T[] Shift<T>(T[] array, int positions)
+        {
+            var copy = new T[array.Length];
+            Array.Copy(array, 0, copy, array.Length - positions, positions);
+            Array.Copy(array, positions, copy, 0, array.Length - positions);
+            return copy;
+        }
+    }
+}
