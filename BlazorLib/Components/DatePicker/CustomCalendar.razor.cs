ed879e6c176acaf1d664efecb3da10777bfc1b88
Walker <sarsey.james@gmailk.com>
2023-12-16 13:01:20 -0300

    Created base class and fixed two way binding

--- BlazorLib/Components/DatePicker/CustomCalendar.razor.cs
+++ BlazorLib/Components/DatePicker/CustomCalendar.razor.cs
@@ -1,46 +1,16 @@
-﻿using Microsoft.AspNetCore.Components;
-using System.Globalization;
-using BlazorLib.Components.Form;
+﻿using System.Globalization;
 
 namespace BlazorLib.Components.DatePicker
 {
-    public partial class CustomCalendar : WFormBaseComponent<DateTime?>
+    public partial class CustomCalendar : WBaseDate
     {
 
-        /// <summary>
-        /// The currently selected date (two-way bindable). If null, then nothing was selected.
-        /// </summary>
-        [Parameter]
-        public DateTime? Date
-        {
-            get => _value;
-            set => SetDate(value, true);
-        }
-
-        /// <summary>
-        /// Defines on which day the week starts. Depends on the value of Culture.
-        /// </summary>
-        [Parameter]
-        public DayOfWeek? FirstDayOfWeek { get; set; } = null;
-
-        /// <summary>
-        /// Fired when the DateFormat changes.
-        /// </summary>
-        [Parameter] 
-        public EventCallback<DateTime?> SelectedDateChanged { get; set; }
-
-        /// <summary>
-        /// If you want do mark some days, pass they here.
-        /// </summary>
-        [Parameter]
-        public IEnumerable<DateTime>? MarkedDays { get; set; }
-
         private DateTime currentDate = DateTime.Today;
-        private DateTime currentMonth => new DateTime(currentDate.Year, currentDate.Month, 1);
+        private DateTime currentMonth => new(currentDate.Year, currentDate.Month, 1);
         private int currentYear => currentMonth.Year;
         private int daysInMonth => DateTime.DaysInMonth(currentYear, currentMonth.Month);
 
-        private List<DateTime> days = null!;
+        private List<DateTime> days = new List<DateTime>();
 
         protected override void OnInitialized()
         {
@@ -66,23 +36,22 @@ namespace BlazorLib.Components.DatePicker
         }
 
 
-        private async void SelectDate(int index)
+        private void SelectDate(int index)
         {
             Date = days[index];
-            await SelectedDateChanged.InvokeAsync(Date);
         }
 
         private void PreviousMonth()
         {
             currentDate = currentDate.AddMonths(-1);
-            SelectedDateChanged.InvokeAsync(currentDate);
+            MonthChanged.InvokeAsync(currentDate);
             UpdateDays();
         }
 
         private void NextMonth()
         {
             currentDate = currentDate.AddMonths(1);
-            SelectedDateChanged.InvokeAsync(currentDate);
+            MonthChanged.InvokeAsync(currentDate);
             UpdateDays();
         }
 
@@ -95,23 +64,6 @@ namespace BlazorLib.Components.DatePicker
             return "";
         }
 
-        protected void SetDate(DateTime? date, bool updateValue)
-        {
-            if (_value != null && date != null && date.Value.Kind == DateTimeKind.Unspecified)
-            {
-                date = DateTime.SpecifyKind(date.Value, _value.Value.Kind);
-            }
-
-            if (_value != date)
-            {
-                if (date is not null)
-                {
-                    _value = date;
-                    return;
-                }
-                _value = date;
-            }
-        }
 
         /// <summary>
         /// Get week names 
---
+++ BlazorLib/Components/DatePicker/WBaseDate.cs
@@ -0,0 +1,68 @@
+﻿using BlazorLib.Components.Form;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorLib.Components.DatePicker
+{
+    public class WBaseDate : WFormBaseComponent<DateTime?>
+    {
+
+        /// <summary>
+        /// The currently selected date (two-way bindable). If null, then nothing was selected.
+        /// </summary>
+        [Parameter]
+        public DateTime? Date
+        {
+            get => _value;
+            set => SetDate(value);
+        }
+
+        protected void SetDate(DateTime? date)
+        {
+            if (_value != null && date != null && date.Value.Kind == DateTimeKind.Unspecified)
+            {
+                date = DateTime.SpecifyKind(date.Value, _value.Value.Kind);
+            }
+
+            if (_value != date)
+            {
+                if (date is not null)
+                {
+                    _value = date;
+                    DateChanged.InvokeAsync(_value);
+                    return;
+                }
+                return;
+            }
+            return;
+        }
+
+        /// <summary>
+        /// Fired when the Date changes.
+        /// </summary>
+        [Parameter]
+        public EventCallback<DateTime?> DateChanged { get; set; }
+
+        /// <summary>
+        /// Defines on which day the week starts. Depends on the value of Culture.
+        /// </summary>
+        [Parameter]
+        public DayOfWeek? FirstDayOfWeek { get; set; } = null;
+
+
+        /// <summary>
+        /// If you want do mark some days, pass they here.
+        /// </summary>
+        [Parameter]
+        public IEnumerable<DateTime>? MarkedDays { get; set; }
+
+        /// <summary>
+        /// Fired when the month of the picker changes.
+        /// <para>Specially useful for when you want mark some specific dates.</para>
+        /// </summary>
+        [Parameter]
+        public EventCallback<DateTime?> MonthChanged { get; set; }
+
+        [Parameter]
+        public string DateFormat { get; set; } = "yyyy-MM-dd";
+    }
+}
